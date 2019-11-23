b33a40356f8eeeb355fb5f3aca62e82fa919422d
Lextrait <lextraitthibault@hotmail.fr>
2019-11-23 23:43:55 +0100

    Mobs prefab

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovements>() != null)
+            transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
