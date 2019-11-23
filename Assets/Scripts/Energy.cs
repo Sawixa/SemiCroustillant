515694860073ba52c9a3576f52c0a9f80ee441d2
Al3xCalibur <montesquieu.amis@hotmail.fr>
2019-11-23 18:25:43 +0100

    energy dash

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -11,6 +11,8 @@ public class Energy : MonoBehaviour
 
     [SerializeField, Range(0,100)] private float _energyLevel;
 
+    public float energy => this._energyLevel;
+
     [Tooltip("Regain d'énergie")]
     [SerializeField] private float _energyRegen;
 
@@ -44,7 +46,8 @@ public class Energy : MonoBehaviour
         if (!dying)
         {
             Debug.Assert(_playerMovements.MaxSpeeds[_playerMovements.MaxSpeeds.Length - 1] > float.Epsilon, "Vitesse maximale nulle.");
-            _energyLevel = Mathf.Clamp(_energyLevel + (_energyRegen - _energyCost.Evaluate(_playerMovements.Speed.magnitude / _playerMovements.MaxSpeeds[_playerMovements.MaxSpeeds.Length - 1])) * Time.deltaTime, 0f, 100f);
+            if(!_playerMovements.IsDashing)
+                _energyLevel = Mathf.Clamp(_energyLevel + (_energyRegen - _energyCost.Evaluate(_playerMovements.Speed.magnitude / _playerMovements.MaxSpeeds[_playerMovements.MaxSpeeds.Length - 1])) * Time.deltaTime, 0f, 100f);
             _light2D.pointLightOuterRadius = _lightRadius.Evaluate(_energyLevel / 100f);
         }
 
@@ -54,6 +57,11 @@ public class Energy : MonoBehaviour
         }
 
 
+    }
+
+    public void add(float x)
+    {
+        _energyLevel += x;
     }
 
     private IEnumerator Die()
