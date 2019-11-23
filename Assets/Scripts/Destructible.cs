334e9b8fef327e6fe3c1d44c1307ae01275d0762
Adrien Fontanesi <adrien.vizier@gmail.com>
2019-11-23 13:28:16 +0100

    first commit on branch

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Destructible : MonoBehaviour
 {
+    public GameObject player;
+
+    private PlayerMovements _playerMov;
+
+    private Collider2D _collider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Assert(player != null);
+        if (player != null)
+        {
+            _playerMov = player.GetComponent<PlayerMovements>();
+            Debug.Assert(_playerMov != null);
+        }
+
     }
 
     // Update is called once per frame
