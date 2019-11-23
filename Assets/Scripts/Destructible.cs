88adece9d3deb81352bbbeb78d6386faa1fc9cc0
Adrien Fontanesi <adrien.vizier@gmail.com>
2019-11-23 21:49:22 +0100

    desctuction of the trunc

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Destructible : MonoBehaviour
 {
     private Collider2D _colliderDestructible;
+    private SpriteRenderer _sprRenderer;
+
+    public Sprite damagedSprite;
+
+    public int nbHitsToDestroy;
+    private int _nbHits = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Assert(damagedSprite != null);
+        Debug.Assert(_nbHits > 0);
         _colliderDestructible = GetComponent<Collider2D>();
+        _sprRenderer = GetComponent<SpriteRenderer>();
     }
 
 
@@ -21,8 +31,18 @@ public class Destructible : MonoBehaviour
         {
             if (playerMovements.IsDashing)
             {
-                _colliderDestructible.enabled = false;
-                // TODO Destroy wall
+                _nbHits++;
+                if (_nbHits == 1)
+                {
+                    _sprRenderer.sprite = damagedSprite;
+                }
+                if (_nbHits >= nbHitsToDestroy)
+                {
+                    _colliderDestructible.enabled = false;
+                    _sprRenderer.enabled = false;
+                    // TODO Destroy wall
+                }
+
             }
         }
     }
