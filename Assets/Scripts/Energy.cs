ff257c1f418a5a189534820e68ac35757bf1b691
Lextrait <lextraitthibault@hotmail.fr>
2019-11-23 08:55:04 +0100

    Ajout script énergie

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy : MonoBehaviour
+{
+
+    [SerializeField, Range(0,100)] private float _energyLevel;
+
+    [SerializeField] private float energyRegen;
+    [SerializeField] private float energyCost;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _energyLevel = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _energyLevel = Mathf.Clamp(_energyLevel + (energyRegen - energyCost)*Time.deltaTime, 0f, 100f);
+    }
+
+
+}
