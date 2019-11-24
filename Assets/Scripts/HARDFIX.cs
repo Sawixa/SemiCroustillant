9c59dcc08026bb5d23be7999a7099dcded2e100d
AlgO <alexandre.brethes@gmail.com>
2019-11-24 15:27:37 +0100

    CUTSCENE FIXED

---
+++ Assets/Scripts/HARDFIX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HARDFIX : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject cutscene;
+    void Start()
+    {
+        StartCoroutine(DestroyCutscene());
+    }
+
+    IEnumerator DestroyCutscene()
+    {
+        yield return new WaitForSeconds(6f);
+        player.SetActive(false);
+        Destroy(cutscene);
+        yield return new WaitForSeconds(.001f);
+        player.SetActive(true);
+    }
+}
