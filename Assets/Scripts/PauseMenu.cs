03ed16197da2a2d1a76f6084c1b80b5e47ec47b8
Adrien Fontanesi <adrien.vizier@gmail.com>
2019-11-24 10:20:23 +0100

    fix (Pause Menu) : button works and prettier

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartCurrentScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
